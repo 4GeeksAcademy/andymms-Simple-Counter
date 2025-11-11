af0487a20ce78834a2db25fa3fd7dd5fdfcd36bc
Andymms <andreamm0714@gmail.com>
2025-11-11 03:02:59 +0000

    Proyecto Finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,23 +4,65 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const Home = (props) => {
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const handleInputChange = (event) => {
+		props.onInputChange(event.target.value)
+	};
+
+	return (
+		<div className="text-center mt-5">
+			<h1 className="text-white">Counter</h1>
+			<div className="d-inline-flex p-2 flex-row-reverse text-white fs-1">
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1>{props.digitOne}</h1>
+				</div>
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1>{props.digitTwo}</h1>
+				</div>
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1>{props.digitThree}</h1>
+				</div>
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1>{props.digitFour}</h1>
+				</div>
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1>{props.digitFive}</h1>
+				</div>
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1>{props.digitSix}</h1>
+				</div>
+				<div className="mx-2 p-4 bg-dark rounded border border-dark-subtle">
+					<h1><i class="fa-solid fa-stopwatch"></i></h1>
+				</div>
+
+			</div>
+			<br></br>
+			<input className="my-3" type="number" placeholder="0"
+				onChange={handleInputChange}></input>
+			<button className="ms-2 btn btn-success" onClick={props.setTimerAction}>
+				Set timer</button>
+			<br></br>
+			<button
+				className="btn btn-danger mx-1"
+				onClick={props.stopAction}>
+				STOP
+			</button>
+			<button
+				className="btn btn-success mx-1"
+				onClick={props.startAction}>
+				RESUME
+			</button>
+			<button
+				className="btn btn-warning mx-1"
+				onClick={props.resetAction}>
+				RESET
+			</button>
+			<br></br>
+			<input className="my-3" type="number" placeholder="0"
+				onChange={(event) => props.onTargetInputChange(event.target.value)}></input>
+			<button className="ms-2 btn btn-success" onClick={props.setTargetAction}>
+				Set alert time</button>
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,112 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
-// index.css'
 import '../styles/index.css'
 
-// components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let root = ReactDOM.createRoot(document.getElementById("root"))
+
+let contador = 0
+let inputStartValue = 0; 
+let timer = null; 
+let targetTime = null;
+let inputTargetValue = 0;
+
+
+function handleTargetInput(value) {
+   const numericValue = parseInt(value, 10);
+  if (!isNaN(numericValue)) {
+    inputTargetValue = numericValue;
+  } else {
+    inputTargetValue = 0; 
+  }
+}
+
+function setTarget() {
+  targetTime = inputTargetValue;
+}
+
+function handleInputEntry(value) {
+  const numericValue = parseInt(value, 10);
+  if (!isNaN(numericValue)) {
+    inputStartValue = numericValue;
+  } else {
+    inputStartValue = 0; 
+  }
+}
+
+function setTimer() {
+  contador = inputStartValue;
+  inputStartValue = 0;
+  startTimer();
+  renderCounter(); 
+}
+
+function stopTimer() {
+  clearInterval(timer);
+  timer = null;
+}
+
+function resetTimer() {
+  stopTimer();
+  contador = 0;
+  inputStartValue = 0;
+  renderCounter();
+}
+
+function startTimer() {
+  if (timer !== null) {
+    return;
+  }
+  
+  timer = setInterval(() => {
+    contador++; 
+    renderCounter(); 
+  }, 1000);
+}
+
+
+function renderCounter() {
+  const six = Math.floor((contador/100000) % 10);
+  const five = Math.floor((contador/10000) % 10);
+  const four = Math.floor((contador/1000) % 10);
+  const three = Math.floor((contador/100) % 10);
+  const two = Math.floor((contador/10) % 10);
+  const one = Math.floor((contador/1) % 10);
+
+  if (targetTime !== null & contador === targetTime) {
+    alert(`You just reached ${targetTime} seconds!`)
+
+    stopTimer();
+    targetTime = null;
+    
+  }
+  
+  root.render(
+    <React.StrictMode>
+      <Home 
+        digitOne={one}
+        digitTwo={two}
+        digitThree={three}
+        digitFour={four}
+        digitFive={five}
+        digitSix={six}
+        onInputChange={handleInputEntry}
+        setTimerAction={setTimer}
+        startAction={startTimer}
+        stopAction={stopTimer}
+        resetAction={resetTimer}
+        onTargetInputChange={handleTargetInput}
+        setTargetAction={setTarget}
+      />
+    </React.StrictMode>
+  );
+}
+
+renderCounter();
+
+startTimer();
